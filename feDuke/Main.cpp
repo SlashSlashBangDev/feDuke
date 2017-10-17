6463d3406ee42c5432528f0d454adb766b54bf92
Taylor Ellington <taylorellington101@gmail.com>
2017-10-17 14:52:53 -0700

    Got libraries working

--- feDuke/Main.cpp
+++ feDuke/Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include "ThirdParty\headers\GLFW\glfw3.h"
+
+
+
 int main(int argc, char** argv) {
+	 
+
+	glfwInit();
+	GLFWwindow * window = glfwCreateWindow(500, 500, "feDuke", NULL, NULL);
+
 
 	std::cout << "Hello, world" << std::endl;
 
